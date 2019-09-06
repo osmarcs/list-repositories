3052d034384c2479007226076e9c0025709a8581
Osmar CS <osmar.costa.s@gmail.com>
2019-09-06 19:43:07 -0300

    :sparkles: styling Main page
    added react-icons

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Title, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title>Hello World</Title>;
+  return (
+    <Container>
+      <Title>
+        <FaGithubAlt />
+        Hello World
+      </Title>
+
+      <Form>
+        <input placeholder="Adicionar Repositorio" />
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
