b458f56ecae9e746b8bdd57f52192203c6ca0c59
Osmar CS <osmar.costa.s@gmail.com>
2019-09-09 23:15:46 -0300

    :sparkles: recovery repository details and issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+  const [repository, setRepository] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(async () => {
+    const repoName = decodeURIComponent(match.params.repository);
+    setLoading(true);
+    const [repositoryXHR, issuesXHR] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      await api.get(`/repos/${repoName}/issues`, {
+        state: 'open',
+        per_page: 5,
+      }),
+    ]);
+
+    setRepository(repositoryXHR.data);
+    setIssues(issuesXHR.data);
+    setLoading(false);
+  }, []);
+
+  return <h1>Repository: {repository.full_name}</h1>;
 }
